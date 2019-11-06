ff0949849dafd8d3f412d39206ac4ed6c01b6c0b
顾清欢 <1586052989@qq.com>
2019-11-06 08:35:41 +0800

    ..

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import axios from "axios";
-import { Switch, Route, HashRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { TabBar, Tabs, NavBar, SearchBar } from "antd-mobile";
 import Index from "./Home/index";
 import Strict from "./Strict/index";
